f92811e6ec2725fc3ed2f2b9cd9f4b1948de7f64
Lauren Koperski <laurenkoperski@gmail.com>
2018-10-23 12:34:49 -0500

    update database models for additional future functionality :)

--- mavAgenda/landing/models.py
+++ mavAgenda/landing/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#class User(models.Model):
-    #userPK = models.IntegerField()
-    #username = models.CharField(max_length=75)
-    #password = models.CharField(max_length=75)
-
 class Degree(models.Model):
     BS = 'Bachelor of Science'
     MS = 'Master of Science'
@@ -15,7 +10,7 @@ class Degree(models.Model):
         (MS, 'Master of Science'),
         (PhDS, 'Doctor of Science'),
     )
-    degree_diploma = models.CharField( max_length=50, choices=DIPLOMA_CHOICE, default=BS)
+    degree_diploma = models.CharField(max_length=50, choices=DIPLOMA_CHOICE, default=BS)
     MAJ = 'Major'
     MIN = 'Minor'
     CON = 'Concentration'
@@ -24,7 +19,7 @@ class Degree(models.Model):
         (MIN, 'Minor'),
         (CON, 'Concentration'),
     )
-    degree_type = models.CharField( max_length=50, choices=TYPE_CHOICE, default=MAJ )
+    degree_type = models.CharField(max_length=50, choices=TYPE_CHOICE, default=MAJ)
     CSCI = 'Computer Science'
     MIS = 'Management Information Systems'
     BIOI = 'Bioinformatics'
@@ -37,18 +32,17 @@ class Degree(models.Model):
         (ITIN, 'IT Innovation'),
         (CYBR, 'Cybersecurity'),
     )
-    degree_track = models.CharField( max_length=50, choices=TRACK_CHOICE, default=CSCI )
+    degree_track = models.CharField(max_length=50, choices=TRACK_CHOICE, default=CSCI)
     degree_users = models.ManyToManyField(User)
 
 class Requirement(models.Model):
-    #req_pk = models.IntegerField()
     req_name = models.CharField(max_length=50)
     req_credits = models.IntegerField()
     req_degrees = models.ManyToManyField(Degree)
 
 class Course(models.Model):
-    #course_pk = models.IntegerField()
     course_name = models.CharField(max_length=75)
+    course_subject = models.CharField(max_length=15)
     course_num = models.CharField(max_length=15)
     A = "All"
     S = "Spring"
@@ -74,8 +68,48 @@ class Course(models.Model):
     course_comment = models.CharField(max_length=200, blank=True)
     course_requirements = models.ForeignKey(Requirement, on_delete=models.CASCADE)
 
+class Campus(models.Model):
+    N = "North Campus"
+    S = "Scott (South) Campus"
+    B = "Center Street Campus"
+
+class Building(models.Model):
+    building_name = models.CharField(max_length=45)
+    building_roomNumber = models.CharField(max_length=8)
+    building_campus = models.ManyToManyField(Campus, on_delete=models.CASCADE)
+
+class Instructor(models.Model):
+    instructor_firstName = models.CharField(max_length=20)
+    instructor_lastName = models.CharField(max_length=30)
+
+class Weekday(models.Model):
+    M = "Monday"
+    T = "Tuesday"
+    W = "Wednesday"
+    R = "Thursday"
+    F = "Friday"
+    S = "Saturday"
+    U = "Sunday"
+    DAY_CHOICE = (
+        (M, "Monday"),
+        (T, "Tuesday"),
+        (W, "Wednesday"),
+        (R, "Thursday"),
+        (F, "Friday"),
+        (S, "Saturday"),
+        (U, "Sunday"),
+    )
+    weekday_name = models.CharField(max_length=10, choices=DAY_CHOICE, default=None)
+
+
+class Offering(models.Model):
+    offering_course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    offering_time = models.CharField(max_length=20)
+    offering_location = models.ForeignKey(Building, on_delete=models.CASCADE)
+    offering_days = models.ManyToManyField(Weekday)
+    offering_instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+
 class Prereq(models.Model):
-    #prereq_pk = models.IntegerField
     C = "Corequisite"
     P = "Prerequisite"
     REQ_CHOICE = (
@@ -95,24 +129,7 @@ class UserPreferences(models.Model):
     pref_summer = models.BooleanField()
     pref_summerMinCredits = models.IntegerField()
     pref_summerMaxCredits = models.IntegerField()
+    pref_nextSSF = models.CharField(max_length=10)
+    pref_nextYear = models.IntegerField()
+    pref_nextSemCredit = models.IntegerField()
     pref_user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-#class UserDegree(models.Model):
-    #user_pk = models.ForeignKey(User, primary_key=True, on_delete=models.CASCADE)
-    #degree_pk = models.ForeignKey(Degree, on_delete=models.CASCADE)
-
-#class DegreeReq(models.Model):
-    #degree_pk = models.ForeignKey(Degree, primary_key=True,on_delete=models.CASCADE)
-    #requirement_pk = models.ForeignKey(Requirement, primary_key=True, on_delete=models.CASCADE)
-
-#class RequirementCourse(models.Model):
-    #requirement_pk=models.ForeignKey(Requirement, primary_key=True, on_delete=models.CASCADE)
-    #course_pk=models.ForeignKey(Course, primary_key=True, on_delete=models.CASCADE)
-
-#class CoursePreqreq(models.Model):
-    #course_pk=models.ForeignKey(Course, primary_key=True, on_delete=models.CASCADE)
-    #prereq_pk=models.ForeignKey(Prereq, primary_key=True, on_delete=models.CASCADE)
-
-#class PrereqCourse(models.Model):
-    #prereq_pk = models.ForeignKey(Prereq, primary_key=True, on_delete=models.CASCADE)
-    #course_pk = models.ForeignKEey(Course, preimary_key=True, on_delete=models.CASCADE)
